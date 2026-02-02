9c30f3a9ef00e9e90ed000a50ffbb5b6a25d05ee
Roshan Louhar <rolouhar@microsoft.com>
2026-02-03 00:33:21 +0530

    Added other solutions solved earlier
    Added solution from the old repo

---
+++ CodingConsole/CodingProblems/DCP.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_interview_problems
+{
+    public class DCP
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            #region Given a list of numbers, return whether any two sums to k.
+            //string Str = "10 15 3 7";
+            //int[] array = Array.ConvertAll(Str.Split(' '), int.Parse);
+            //int k = 17;
+            //bool result = FindSumInArray(array , k);
+            #endregion
+
+            #region Given an array of integers return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at self.
+            //string Str = "10 3 5 6 2";
+            //int[] array = Array.ConvertAll(Str.Split(' '), int.Parse);
+            //FindProductArrayExceptSelf(array);
+            #endregion
+
+            #region // This function finds the buy sell schedule for maximum profit
+            // stock prices on consecutive days
+            //var price = new int[] { 100, 180, 260, 310, 40, 535, 695 };
+            // function call
+            //StockBuySell(price);
+            #endregion
+
+            #region Given a list of numbers, return whether any two sums to k.
+            int k = 100;
+            int result = FindSumOfPrimeNumbers(k);
+            #endregion
+
+
+
+
+            Console.ReadKey();
+        }
+
+        private static int FindSumOfPrimeNumbers(int k)
+        {
+            int sum = 1;
+            int ctr = 0, n = 0;
+
+            while (ctr < 100)
+            {
+                n++;
+                if (n % 2 != 0)
+                {
+                    // check if the number is even
+                    if (Is_Prime(n))
+                    {
+                        sum += n;
+                    }
+                }
+                ctr++;
+            }
+
+
+            //int sum = 0;
+            //bool[] tempArr = new bool[k + 1];
+            //for (int i = 2; i * i <= k; i++)
+            //{
+            //    if (tempArr[i] == false)
+            //    {
+            //        for (int j = i * 2; j <= k;)
+            //        {
+            //            tempArr[j] = true;
+            //            j += i;
+            //        }
+            //    }
+            //}
+            //for (int p = 2; p <= k; p++)
+            //    if (!tempArr[p])
+            //        sum += p;
+
+            return sum;
+        }
+
+        private static bool Is_Prime(int n)
+        {
+            for (int i = 3; i * i <= n; i += 2)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private static void StockBuySell(int[] price)
+        {
+            // Prices must be given for at least two days
+            if (price.Length == 1)
+                return;
+
+            // solution array
+            var sol = new List<Interval>();
+
+            var buyIndex = -1;
+
+            for (int i = 0; i < price.Length - 1; i++)
+            {
+                if (buyIndex < 0)
+                {
+                    if (price[i] <= price[i + 1])
+                        buyIndex = i;
+                }
+                else
+                {
+                    if (price[i] > price[i + 1] || i + 1 == price.Length - 1)
+                    {
+                        sol.Add(new Interval { Buy = buyIndex, Sell = i });
+                        buyIndex = -1;
+                    }
+                }
+            }
+
+            // print solution
+            if (sol.Count == 0)
+                Console.WriteLine("There is no day when buying the stock "
+                + "will make profit");
+            else
+                for (int j = 0; j < sol.Count; j++)
+                    Console.WriteLine("Buy on day: " + sol[j].Buy
+                    + " "
+                    + "Sell on day : " + sol[j].Sell);
+
+        }
+
+        private static void FindProductArrayExceptSelf(int[] array)
+        {
+            int size = array.Length;
+            int[] ProdArray = new int[size];
+
+            //int[] Left = new int[size];
+            //int[] Right = new int[size];
+            //Left[0] = 1;
+            //Right[size - 1] = 1;
+            //for (int i = 1; i < size; i++)
+            //{
+            //    Left[i] = Left[i - 1] * array[i - 1];
+            //}
+            //for (int i = size - 2; i >= 0; i--)
+            //{
+            //    Right[i] = Right[i + 1] * array[i + 1];
+            //}
+            //for (int i = 0; i < size; i++)
+            //{
+            //    ProdArray[i] = Left[i] * Right[i];
+            //}
+
+            int i, temp = 1;
+
+            for (i = 0; i < size; i++)
+            {
+                ProdArray[i] = 1;
+            }
+
+            for (i = 0; i < size; i++)
+            {
+                ProdArray[i] = temp;
+                temp = temp * array[i];
+            }
+
+            temp = 1;
+            for (i = size - 1; i >= 0; i--)
+            {
+                ProdArray[i] = ProdArray[i] * temp;
+                temp = temp * array[i];
+            }
+
+
+            for (i = 0; i < size; i++)
+            {
+                Console.Write(ProdArray[i] + " ");
+            }
+
+
+        }
+
+        private static bool FindSumInArray(int[] array, int k)
+        {
+            bool result = false;
+            HashSet<int> set = new HashSet<int>();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                int SubNumber = Math.Abs(k - array[i]);
+                if (set.Contains(SubNumber))
+                {
+                    result = true;
+                    break;
+                }
+                else
+                {
+                    set.Add(array[i]);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    class Interval
+    {
+        public int Buy { get; set; }
+        public int Sell { get; set; }
+    }
+}
---
+++ CodingConsole/CodingProblems/testProblems.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_interview_problems
+{
+    /// <summary>
+    /// interview problems collection
+    /// </summary>
+    internal class testProblems
+    {
+        static void Main(string[] args)
+        {
+            #region biggest sum 
+            //Console.WriteLine("Hello World!");
+            //int[] prices = { 1, 3, 3, 2, 5 };
+            //int sum = biggestSum(intArr);
+            #endregion
+
+            #region find final price salesman problem 
+            //List<int> list = new List<int>();
+            //list.Add(2);
+            //list.Add(3);
+            //list.Add(1);
+            //list.Add(2);
+            //list.Add(4);
+            //list.Add(2);
+            //list.Add(1);
+            //list.Add(3);
+            //list.Add(3);
+            //list.Add(2);
+            //list.Add(5);
+            //finalPrice(list);
+            #endregion
+
+            #region find excel column
+            //int number = 100;
+            // FindColumnName(number);
+            #endregion
+
+            #region find max substring without repeated char in string
+            //string str = "GOOGLEBABAKIJAYHO";
+            //FindMaxSubstringNonRepeatedChar(str);
+            #endregion
+
+            #region put the linkedlist in specific order like 1 , n ,2 ,n-1 ,3 , n-2.....4
+            //ArrangeLinkedListAlternateOrder();
+            #endregion
+
+            #region longest rising start problem @ Okra Solar
+            //LongestRisingSequence();
+            #endregion
+
+            #region PrintFizzBuzzLogic
+            //int count = Convert.ToInt32(Console.ReadLine());
+            //string str = Console.ReadLine();
+            //int[] numbers = Array.ConvertAll(str.Split(' '), int.Parse);
+
+            //for (int i = 1; i < count; i++)
+            //{
+            //    PrintFizzBuzzLogic(numbers[i - 1]);
+            //}
+            #endregion
+
+            #region repeating element in array with O(n) time and O(1) space
+            //RepeatingElements();
+            #endregion
+
+
+            #region cimpress getting profit gain
+            //int NoOfVillage = 100; //Convert.ToInt32(Console.ReadLine());
+            //string VillageProfilestr = "1 3 13 31 33 34 44 46 50 62 64 65 69 70 74 75 77 94 105 108 109 111 114 115 116 126 128 132 139 145 146 149 153 157 158 164 166 175 178 187 188 195 197 198 201 204 207 208 209 210 212 218 222 237 242 244 247 252 255 263 264 266 273 276 277 284 292 294 300 308 325 335 338 342 346 348 351 352 353 354 356 365 370 373 382 384 385 398 405 410 426 427 428 447 448 455 457 459 465 497"; //Console.ReadLine();
+            //int[] Profit = Array.ConvertAll(VillageProfilestr.Split(' '), int.Parse);
+            //int MaxProfit = GetMaxProfit(Profit);
+            //Console.WriteLine(MaxProfit);
+
+            #endregion
+
+
+            Console.ReadKey();
+        }
+
+        private static int GetMaxProfit(int[] Profit)
+        {
+            int size = Profit.Length;
+
+            int i = 0, j = 0;
+            Queue<int> AllMaxProfit = new Queue<int>();
+
+            //Queue<int> queueProfit = new Queue<int>();
+            int prevProfit = 0;
+
+            for (j = 0; j < size; j++)
+            {
+                Queue<int> TempProfit = new Queue<int>();
+                for (i = 0 + j; i < size; i++)
+                {
+                    if (i == j)
+                    {
+                        TempProfit.Enqueue(Profit[i]);
+                        prevProfit = Profit[i];
+                    }
+                    else
+                    {
+                        if ((Profit[i] % prevProfit == 0))
+                        {
+                            TempProfit.Enqueue(Profit[i]);
+                            prevProfit = Profit[i];
+                        }
+                    }
+                }
+                AllMaxProfit.Enqueue(TempProfit.Sum());
+            }
+
+            return AllMaxProfit.Max();
+        }
+
+        private static void RepeatingElements()
+        {
+            int[] numRay1 = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
+            Console.Write("The repeating" + " elements are : ");
+            for (int i = 0; i < numRay1.Length; i++)
+            {
+                int temp = Math.Abs(numRay1[i]);
+                if (numRay1[temp] >= 0)
+                    numRay1[temp] = -numRay1[temp];
+                else
+                    Console.Write(Math.Abs(numRay1[i]) + " ");
+            }
+
+            ////Method-1 handle zero case also
+            //for (int i = 0; i < numRay1.Length; i++)
+            //{
+            //    int temp = (numRay1[i] % 10);
+            //    numRay1[temp] = numRay1[temp] + 10;
+            //}
+            //Console.WriteLine("The repeating elements are : ");
+            //for (int i = 0; i < numRay1.Length; i++)
+            //    if (numRay1[i] > 19)
+            //        Console.WriteLine(i + " ");
+        }
+
+        private static void PrintFizzBuzzLogic(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        private static void LongestRisingSequence()
+        {
+            int[] array = { 0, -100, 0, 1, -2, 3, 4, 10, -4, 3, 3, 2, 1, 2, 3, -1, 200, 305 };
+            //{ -100, 0, 1, -2, 3, 4, 10, -4, 3, 3, 2, 1, 2, 3, -1, 200, 305 }
+
+            Queue<int> queue = new Queue<int>();
+
+            int currentMax = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < currentMax)
+                {
+                    queue.Enqueue(i);
+                }
+                currentMax = array[i];
+            }
+
+            while (queue.Count > 0)
+            {
+                Console.Write(queue.Dequeue() + "  ");
+            }
+
+        }
+
+        public static void ArrangeLinkedListAlternateOrder()
+        {
+            Console.WriteLine("Before Linked list is : ");
+            List<int> listData = new List<int>();
+            listData.Add(1);
+            listData.Add(2);
+            listData.Add(3);
+            listData.Add(4);
+            listData.Add(5);
+            listData.Add(6);
+
+            foreach (var item in listData)
+            { Console.Write(item + " "); }
+
+            List<int> NewList = new List<int>();
+
+            int i = 0;
+            while (listData.Count > 0)
+            {
+                if (i % 2 != 0)
+                {
+                    NewList.Add(listData.Last());
+                    listData.Remove(listData.LastOrDefault());
+                }
+                else
+                {
+                    NewList.Add(listData.First());
+                    listData.Remove(listData.FirstOrDefault());
+                }
+                i++;
+            }
+
+            Console.WriteLine("\nAfter Linked list is : ");
+            foreach (var item in NewList)
+            { Console.Write(item + " "); }
+
+        }
+
+        public static void FindMaxSubstringNonRepeatedChar(string str)
+        {
+            Console.WriteLine("Main string is : {0}", str);
+
+            Dictionary<char, int> DictChar = new Dictionary<char, int>();
+
+
+            int MaxLength = 0, index = 0; string MaxString = string.Empty;
+            for (int i = 0; i < str.Length;)
+            {
+                if (DictChar.ContainsKey(str[i]))
+                {
+                    i = DictChar[str[i]] + 1;
+                    index = DictChar.Count;
+                    if (MaxLength < index)
+                    {
+                        MaxLength = DictChar.Count;
+                        MaxString = string.Join("", DictChar.Select(x => x.Key).ToArray());
+                    }
+                    DictChar.Clear();
+                }
+                else
+                {
+                    DictChar.Add(str[i], i);
+                    i++;
+                }
+            }
+
+
+            Console.WriteLine("Length of the string :{0}, Max substring is :{1}", MaxLength, MaxString);
+
+        }
+
+        public static void FindColumnName(int number)
+        {
+            char[] Alphabhets = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                 'Q','R','S','T','U','V','W','X','Y','Z' };
+            string result = "";
+            Stack<int> charNumber = new Stack<int>();
+
+            if (number < 26)
+            {
+                Console.WriteLine(Alphabhets[number]);
+                return;
+            }
+            int reminder = 0, divisor = number;
+            while (divisor > 26)
+            {
+                reminder = divisor % 26;
+                divisor = divisor / 26;
+                if (reminder == 0)
+                {
+                    divisor = divisor - 1;
+                    charNumber.Push(26);
+                }
+                else
+                {
+                    charNumber.Push(reminder);
+                }
+
+            }
+            charNumber.Push(divisor);
+
+            while (charNumber.Count > 0)
+            {
+                Console.Write(Alphabhets[charNumber.Pop() - 1]);
+            }
+            return;
+        }
+
+        public static int biggestSum(int[] arr)
+        {
+            int globalSum = arr[0], localSum = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                localSum = Math.Max(arr[i], globalSum + arr[i]);
+                globalSum = Math.Max(localSum, globalSum);
+            }
+
+            return globalSum;
+        }
+
+        public static void finalPrice(List<int> prices)
+        {
+            int sum = 0;
+            //int index = 0;
+            string noOutput = "";
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(prices[prices.Count - 1]);
+            sum = sum + prices[prices.Count - 1];
+            noOutput = noOutput + (prices.Count - 1) + " ";
+
+
+            for (int i = prices.Count - 2; i >= 0; i--)
+            {
+                int firstmin = 0;
+                while (queue.Count > 0)
+                {
+                    int temp = queue.Dequeue();
+                    if (temp <= prices[i])
+                    {
+                        firstmin = temp;
+                    }
+                }
+                if (firstmin < prices[i])
+                {
+                    sum = sum + (prices[i] - firstmin);
+                }
+                else
+                {
+                    sum = sum + (prices[i]);
+                }
+
+                if (firstmin <= 0)
+                    noOutput = noOutput + (i) + " ";
+
+
+                if (queue.Count <= 0)
+                {
+                    queue.Enqueue(prices[i]);
+                }
+
+            }
+
+            char[] IndexOutput = noOutput.Trim().ToCharArray();
+            Array.Reverse(IndexOutput);
+            string temp1 = new string(IndexOutput);
+
+            Console.WriteLine(sum);
+            Console.WriteLine(temp1);
+
+        }
+
+        //public static void finalPrice(List<int> prices)
+        //{
+        //    int sum = 0;
+        //    int index = 0;
+        //    string noOutput = "";
+
+        //    for (int i = 0; i < prices.Count - 1; i++)
+        //    {
+        //        int rightMin = prices[i];
+        //        index = int.MaxValue;
+        //        for (int j = i + 1; j < prices.Count; j++)
+        //        {
+        //            if (rightMin >= prices[j])
+        //            {
+        //                rightMin = prices[j];
+        //                index = j;                        
+        //            }
+        //        }
+
+        //        if (prices[i] == rightMin)
+        //        {
+        //            sum = sum + prices[i];
+        //            if (index == int.MaxValue)
+        //                noOutput = noOutput + i.ToString() + " ";
+        //        }
+        //        else
+        //        {
+        //            sum = sum + (prices[i] - rightMin);
+        //        }
+
+        //    }
+        //    sum = sum + prices[prices.Count - 1];
+        //    noOutput = noOutput + (prices.Count - 1).ToString();
+
+        //    Console.WriteLine(sum);
+        //    Console.WriteLine(noOutput.Trim());
+
+        //}
+    }
+}
